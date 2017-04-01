a3c145deaa14c92b64ae02ac1ca8987de16f45e1
Coen Zimmerman <coen@pixelindustries.com>
2017-04-01 11:57:12 +0200

    Cleaned up jsonapi (form)request

--- src/Http/Requests/JsonApiRequest.php
+++ src/Http/Requests/JsonApiRequest.php
@@ -1,16 +1,17 @@
 <?php
 namespace Pixelindustries\JsonApi\Http\Requests;
 
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Request;
 use Pixelindustries\JsonApi\Support\Request\RequestQueryParser;
 
-class JsonApiRequest extends Request
+class JsonApiRequest extends FormRequest
 {
 
     /**
      * @var RequestQueryParser
      */
-    protected $jsonApiParser;
+    protected $jsonApiQuery;
 
     /**
      * {@inheritdoc}
@@ -19,63 +20,16 @@ class JsonApiRequest extends Request
     {
         parent::__construct();
 
-        $this->jsonApiParser = new RequestQueryParser($this);
+        $this->jsonApiQuery = new RequestQueryParser($this);
     }
 
     /**
      * @return RequestQueryParser
      */
-    public function jsonApi()
+    public function jsonApiQuery()
     {
-        return $this->jsonApiParser;
+        return $this->jsonApiQuery;
     }
 
-    /**
-     * Returns full filter data.
-     *
-     * @return array
-     */
-    public function jsonApiFilter()
-    {
-        return $this->jsonApiParser->getFilter();
-    }
-
-    /**
-     * Returns a specific key's value from the filter.
-     *
-     * @param string $key
-     * @param mixed  $default
-     * @return mixed
-     */
-    public function jsonApiFilterValue($key, $default = null)
-    {
-        return $this->jsonApiParser->getFilterValue($key, $default);
-    }
-
-    /**
-     * Returns full page data.
-     *
-     * @return array
-     */
-    public function jsonApiPageData()
-    {
-        return $this->jsonApiParser->getPageData();
-    }
-
-    /**
-     * @return int
-     */
-    public function jsonApiPageNumber()
-    {
-        return $this->jsonApiParser->getPageNumber();
-    }
-
-    /**
-     * @return int
-     */
-    public function jsonApiPageSize()
-    {
-        return $this->jsonApiParser->getPageSize();
-    }
 
 }
